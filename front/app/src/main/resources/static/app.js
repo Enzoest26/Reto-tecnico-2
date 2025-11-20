892911fd2374564eca8f09ed6293ebb278932b90
Enzo Jesús Esteban Quispe <enzoest26@gmail.com>
2025-11-20 00:31:41 -0500

    feat: add front spring app

---
+++ front/app/src/main/resources/static/app.js
@@ -0,0 +1,15 @@
+function initialLoad() {
+    //const url = `http://${window.location.hostname}:30081`;
+
+    fetch('/api/products')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('product-list').innerText = JSON.stringify(data);
+            console.log('Data received:', data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+initialLoad();
